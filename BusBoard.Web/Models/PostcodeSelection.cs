f145f0f8cf0871ef17233836a6e01e0fa86610b1
Emily Patsko <emily.patsko@softwire.com>
2022-07-08 15:43:32 +0100

    style: Add extra styling and validation

--- BusBoard.Web/Models/PostcodeSelection.cs
+++ BusBoard.Web/Models/PostcodeSelection.cs
@@ -1,7 +1,12 @@
-﻿namespace BusBoard.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusBoard.Web.Models
 {
     public class PostcodeSelection
     {
+        [RegularExpression(@"^(?i)([A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$",
+            ErrorMessage = "Please enter a valid postcode.")]
+        [Required(ErrorMessage = "You must enter a postcode.", AllowEmptyStrings = false)]
         public string Postcode { get; set; }
 
     }
